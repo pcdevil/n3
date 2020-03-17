987ca0fde399267fa7c3d480bb6a6df93c7725cb
Attila Gonda <pcdevil@users.noreply.github.com>
2020-03-17 14:04:08 +0100

    feat(npm-tasks): add aliases

--- tasks/npm-tasks.mk
+++ tasks/npm-tasks.mk
@@ -1,20 +1,37 @@
-.PHONY: lint start start-watch test test-watch .npm-task .npm-install-dev-dependencies
+.PHONY: l s s-w t t-w
+.PHONY: lint start start-watch test test-watch
+.PHONY: .npm-task .npm-install-dev-dependencies
+
+l: ##  -> lint
+l: lint
 
 lint: ## run the "lint" npm task
 lint: NPM_TASK := lint
 lint: .npm-task
 
+s: ##  -> start
+s: start
+
 start: ## run the "start" npm task
 start: NPM_TASK := start
 start: .npm-task
+
+s-w: ##  -> start-watch
+s-w: start-watch
 
 start-watch: ## run the "start:watch" npm task
 start-watch: NPM_TASK := start:watch
 start-watch: .npm-task
 
+t: ##  -> test
+t: test
+
 test: ## run the "test" npm task
 test: NPM_TASK := test
 test: .npm-task
+
+t-w: ##  -> test-watch
+t-w: test-watch
 
 test-watch: ## run the "test:watch" npm task
 test-watch: NPM_TASK := test:watch
