22ae38e9c90df0fb62badd9b0d01acbf36d13892
Attila Gonda <pcdevil@users.noreply.github.com>
2020-04-01 12:05:44 +0200

    fix(npm-tasks): make tasks chainable

--- tasks/install-test.mk
+++ tasks/install-test.mk
@@ -5,7 +5,8 @@ install-test: ## install test framework mocha with chai and sinon included
 install-test: .install-test-packages .install-test-copy-setup .install-test-npm-tasks
 
 .install-test-packages: NPM_PACKAGES := chai mocha sinon sinon-chai
-.install-test-packages: .npm-install-dev-dependencies
+.install-test-packages:
+	$(npm-install-dev-dependencies)
 
 .install-test-copy-setup:
 	mkdir --parents ${PROJECT_DIR}/test
--- tasks/npm-tasks.mk
+++ tasks/npm-tasks.mk
@@ -1,46 +1,52 @@
 .PHONY: l s s-w t t-w
 .PHONY: lint start start-watch test test-watch
-.PHONY: .npm-task .npm-install-dev-dependencies
 
 NPM := $(shell which npm)
 
 lint: ## run the "lint" npm task
 lint: NPM_TASK := lint
-lint: .npm-task
+lint:
+	$(npm-task)
 
 l: ##  -> lint
 l: lint
 
 start: ## run the "start" npm task
 start: NPM_TASK := start
-start: .npm-task
+start:
+	$(npm-task)
 
 s: ##  -> start
 s: start
 
 start-watch: ## run the "start:watch" npm task
 start-watch: NPM_TASK := start:watch
-start-watch: .npm-task
+start-watch:
+	$(npm-task)
 
 s-w: ##  -> start-watch
 s-w: start-watch
 
 test: ## run the "test" npm task
 test: NPM_TASK := test
-test: .npm-task
+test:
+	$(npm-task)
 
 t: ##  -> test
 t: test
 
 test-watch: ## run the "test:watch" npm task
 test-watch: NPM_TASK := test:watch
-test-watch: .npm-task
+test-watch:
+	$(npm-task)
 
 t-w: ##  -> test-watch
 t-w: test-watch
 
-.npm-task:
+define npm-task =
 	$(NPM) run ${NPM_TASK}
+endef
 
-.npm-install-dev-dependencies:
+define npm-install-dev-dependencies =
 	$(NPM) install --save-dev ${NPM_PACKAGES}
+endef
