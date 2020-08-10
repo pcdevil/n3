37bcc6262ddaf4b1c56138c48384008460c4993d
Attila Gonda <pcdevil@users.noreply.github.com>
2020-08-10 17:52:49 +0200

    feat(eslint): use external plugin for jsdoc instead of deprecated internal rules

--- tasks/install-lint.mk
+++ tasks/install-lint.mk
@@ -4,7 +4,7 @@
 install-lint: ## install lint framework eslint with google rules
 install-lint: .install-lint-packages .install-lint-copy-setup .install-lint-npm-tasks
 
-.install-lint-packages: NPM_PACKAGES := eslint eslint-config-google
+.install-lint-packages: NPM_PACKAGES := eslint eslint-config-google eslint-plugin-jsdoc
 .install-lint-packages:
 	$(npm-install-dev-dependencies)
 
