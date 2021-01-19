35114f1e6ffd5fa8c71a17ad9772d9aa2f1bdb58
Attila Gonda <pcdevil@users.noreply.github.com>
2021-01-19 20:09:10 +0100

    refactor(install-lint): rename variable to avoid ambiguousity

--- tasks/install-lint.mk
+++ tasks/install-lint.mk
@@ -3,15 +3,15 @@ LINT_CONFIGS ?= google
 LINT_PLUGINS ?= jsdoc
 LINT_CONFIG_FILES = $(foreach config,${LINT_CONFIGS},${ASSETS_DIR}/eslintrc.config-${config}.json)
 LINT_PLUGIN_FILES = $(foreach plugin,${LINT_PLUGINS},${ASSETS_DIR}/eslintrc.plugin-${plugin}.json)
-LINT_CONFIG_PACKAGES = $(foreach config,${LINT_CONFIGS},eslint-config-${config})
-LINT_PLUGIN_PACKAGES = $(foreach plugin,${LINT_PLUGINS},eslint-plugin-${plugin})
+LINT_CONFIG_NPM_PACKAGES = $(foreach config,${LINT_CONFIGS},eslint-config-${config})
+LINT_PLUGIN_NPM_PACKAGES = $(foreach plugin,${LINT_PLUGINS},eslint-plugin-${plugin})
 
 .PHONY: install-lint
 install-lint: ## install lint framework eslint with google rules
 install-lint: .install-lint-packages .install-lint-copy-setup .install-lint-npm-tasks
 
 .PHONY: .install-lint-packages
-.install-lint-packages: NPM_PACKAGES := eslint ${LINT_CONFIG_PACKAGES} ${LINT_PLUGIN_PACKAGES}
+.install-lint-packages: NPM_PACKAGES := eslint ${LINT_CONFIG_NPM_PACKAGES} ${LINT_PLUGIN_NPM_PACKAGES}
 .install-lint-packages:
 	$(npm-install-dev-dependencies)
 
